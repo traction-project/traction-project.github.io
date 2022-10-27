513d102999e2a44aaad0a7f6a4bd9a1ba4fb62a5
Thomas Röggla <t.roggla@cwi.nl>
2022-10-27 15:37:47 +0200

    fetch_readmes script now combines readme with custom front matter

--- fetch_readmes.sh
+++ fetch_readmes.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-mkdir -p content/co-creation-space
-mkdir -p content/face-and-object-recognition
-mkdir -p content/co-creation-stage
-mkdir -p content/encoding-api
-mkdir -p content/pre-recorded-content-adaptation
-mkdir -p content/live-content-adaptation
-mkdir -p content/immersive-adaptive-player
+function fetch_readme() {
+  mkdir -p content/${2}
+  curl -L https://github.com/traction-project/${1}/raw/master/README.md > content/${2}/readme.md
 
-curl -L https://github.com/traction-project/CoCreationSpace/raw/master/README.md > content/co-creation-space/_index.md
-curl -L https://github.com/traction-project/FaceObjRecognitionForCCSpace/raw/master/README.md > content/face-and-object-recognition/_index.md
-curl -L https://github.com/traction-project/CoCreationStage/raw/master/README.md > content/co-creation-stage/_index.md
-curl -L https://github.com/traction-project/encoding-api/raw/master/README.md > content/encoding-api/_index.md
-curl -L https://github.com/traction-project/PreRecordedContentAdaptation/raw/master/README.md > content/pre-recorded-content-adaptation/_index.md
-curl -L https://github.com/traction-project/LiveContentAdaptation/raw/master/README.md > content/live-content-adaptation/_index.md
-curl -L https://github.com/traction-project/ImmersiveAdaptivePlayer/raw/master/README.md > content/immersive-adaptive-player/_index.md
+  cat content/${2}/{header,readme,footer}.md > content/${2}/_index.md
+}
+
+fetch_readme "CoCreationSpace" "co-creation-space"
+fetch_readme "FaceObjRecognitionForCCSpace" "face-and-object-recognition"
+fetch_readme "CoCreationStage" "co-creation-stage"
+fetch_readme "encoding-api" "encoding-api"
+fetch_readme "PreRecordedContentAdaptation" "pre-recorded-content-adaptation"
+fetch_readme "LiveContentAdaptation" "live-content-adaptation"
+fetch_readme "ImmersiveAdaptivePlayer" "immersive-adaptive-player"
