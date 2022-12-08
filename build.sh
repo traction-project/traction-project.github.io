11048b5d498306395fbcfbdfdf2db6a7acc15f44
Thomas Röggla <t.roggla@cwi.nl>
2022-12-08 15:03:26 +0100

    downloading bulma as part of the build process

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if ! [ -d "assets/styles/bulma" ]; then
+  echo "Downloading Bulma SASS sources..."
+  cd assets/styles
+
+  curl -L https://github.com/jgthms/bulma/releases/download/0.9.4/bulma-0.9.4.zip > bulma.zip
+  unzip bulma.zip
+  rm bulma.zip
+
+  cd ../..
+fi
+
 bash build_sections.sh fetch
 
 if [ "$1" = "serve" ]; then
